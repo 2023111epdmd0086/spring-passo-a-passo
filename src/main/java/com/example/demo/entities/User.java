334fa72c2c0f5f5bd2b3b52855c2b28e42ea822f
Jonas IFPI <catce.2023111EPDMD0086@aluno.ifpi.edu.br>
2023-08-04 17:36:01 -0300

    Adicionado postgresql para testar posteriormente, e feito update do README.md

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -2,10 +2,11 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 
 @Entity(name = "tb_users")
 public class User implements Serializable {
